9e2270ad70f1bc4a449c8de543c64cdf8d3d3f78
mojtaba1989 <mbahramg@mtu.edu>
2024-08-09 09:12:57 -0400

    Fixed issue due to waypoint truncation

--- src/apsrc_udp_data_sharing_nodelet.cpp
+++ src/apsrc_udp_data_sharing_nodelet.cpp
@@ -203,8 +203,10 @@ void ApsrcUdpDataSharingNl::hereAppCallback(const apsrc_msgs::Response::ConstPtr
     }
 
     for (size_t span_id = 0; span_id < msg->routes[ridx].spans.spans.size(); span_id++){
-      udp_msg.here_waypoints[msg->routes[ridx].spans.spans[span_id].offset].speed = msg->routes[ridx].spans.spans[span_id].trafficSpeed; 
-      udp_msg.here_waypoints[msg->routes[ridx].spans.spans[span_id].offset].base_speed = msg->routes[ridx].spans.spans[span_id].baseSpeed; 
+      if (msg->routes[ridx].spans.spans[span_id].offset < 2665){
+        udp_msg.here_waypoints[msg->routes[ridx].spans.spans[span_id].offset].speed = msg->routes[ridx].spans.spans[span_id].trafficSpeed; 
+        udp_msg.here_waypoints[msg->routes[ridx].spans.spans[span_id].offset].base_speed = msg->routes[ridx].spans.spans[span_id].baseSpeed;
+      }
     }
 
     for (size_t span_id = 1; span_id < max_id; span_id++){
@@ -215,26 +217,28 @@ void ApsrcUdpDataSharingNl::hereAppCallback(const apsrc_msgs::Response::ConstPtr
     }
 
     for (size_t action_id = 0; action_id < msg->routes[ridx].actions.actions.size(); action_id++){
-      std::string lower_instruction = toLowerCase(msg->routes[ridx].actions.actions[action_id].instruction);
-      if (lower_instruction.find("ramp") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 2;
-      } else  if (lower_instruction.find("turn left") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 3;
-      } else  if (lower_instruction.find("turn right") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 4;
-      } else  if (lower_instruction.find("exit") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 5;
-      } else  if (lower_instruction.find("roundabout") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 6;
-      } else  if (lower_instruction.find("u-turn") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 7;
-      } else  if (lower_instruction.find("continue") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 8;
-      } else  if (lower_instruction.find("keep") != std::string::npos){
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 8;
-      } else {
-        udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 1;
-      } 
+      if (msg->routes[ridx].actions.actions[action_id].offset < 2665){
+        std::string lower_instruction = toLowerCase(msg->routes[ridx].actions.actions[action_id].instruction);
+        if (lower_instruction.find("ramp") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 2;
+        } else  if (lower_instruction.find("turn left") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 3;
+        } else  if (lower_instruction.find("turn right") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 4;
+        } else  if (lower_instruction.find("exit") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 5;
+        } else  if (lower_instruction.find("roundabout") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 6;
+        } else  if (lower_instruction.find("u-turn") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 7;
+        } else  if (lower_instruction.find("continue") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 8;
+        } else  if (lower_instruction.find("keep") != std::string::npos){
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 8;
+        } else {
+          udp_msg.here_waypoints[msg->routes[ridx].actions.actions[action_id].offset].action = 1;
+        }
+      }
     }
 
     udp_interface_.write(udp_msg.pack());
